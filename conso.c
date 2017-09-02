ed2b8fe6993827ebad14d268e8d1783ee0defa73
RASIKAMAHENDRAN <31558910+RASIKAMAHENDRAN@users.noreply.github.com>
2017-09-02 12:05:16 +0530

    Update conso.c

--- conso.c
+++ conso.c
@@ -3,7 +3,7 @@ void main()
 {
 char ch;
 scanf("%s",ch);
-if(ch==a||ch==e||ch==i||ch==o||ch==u||ch==A||ch==E||ch==I||ch==O||ch==U)
+if((ch=='a') || (ch=='e') || (ch=='i') || (ch=='o')|| (ch=='u') ||(ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
 {
 printf("vowels");
 }
