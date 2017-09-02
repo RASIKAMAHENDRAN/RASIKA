8b16ce2b32ad465e53a8a11c5b1e3910403af154
RASIKAMAHENDRAN <31558910+RASIKAMAHENDRAN@users.noreply.github.com>
2017-09-02 15:06:58 +0530

    Create eve-in.c

---
+++ eve-in.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+      
+      
+        
+void main()
+      
+      
+        {
+      
+      
+        int l,u,i;
+      
+      
+        scanf("%d %d",&l,&u);
+      
+      
+        for(i=l;i<=u;i++)
+      
+      
+        {
+      
+      
+        if(i%2==0)
+      
+      
+        {
+      
+      
+        printf("%d",i);
+      
+      
+        }
+      
+      
+        }
+      
+      
+        }
