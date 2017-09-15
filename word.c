0d2ec757e0eccf4096bb2eaa992a334d94094359
RASIKAMAHENDRAN <31558910+RASIKAMAHENDRAN@users.noreply.github.com>
2017-09-15 14:18:59 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int c;
+if(c==1)
+{
+printf("no sentence");
+}
+else
+{
+for (i=1;i<a[];i++)
+{
+printf("words are\n",a[i]);
+}
+}
+printf("total words are : %d",c-1);
+}
