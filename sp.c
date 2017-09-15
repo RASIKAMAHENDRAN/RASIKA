01f023d94970bef666ab6fef3b6e5ad6ed71eb89
RASIKAMAHENDRAN <31558910+RASIKAMAHENDRAN@users.noreply.github.com>
2017-09-15 14:36:50 +0530

    Create sp.c

---
+++ sp.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int nc=sc=0;
+scanf("%S",a[]);
+for(i=0;i<=a[];i++)
+{
+if((a[i]>=0 )&&(a[i]<=9))
+{
+nc++;
+}
+else if(a[i]==' ')
+{
+printf("space");
+}
+elseif(((a[i]>=A)&&(a[i]<=Z)||((a[i]>=a)&&(a[i]<=z)))
+{
+printf("character");
+}
+else
+{
+sc++;
+}
+
+printf("%d",sc);
+}
