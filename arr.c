7646d12b1aa500a88c860011259009546cef4d73
RASIKAMAHENDRAN <31558910+RASIKAMAHENDRAN@users.noreply.github.com>
2017-09-30 14:25:45 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n,s=0;
+flaot av;
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=ni++)
+{
+sum=sum+a[0];
+}
+av=sum/n;
+printf("%.2f",av);
+}
