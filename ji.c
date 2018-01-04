705b226e0ab0027dd29bd7cf0d842b181509eecf
RASIKAMAHENDRAN <31558910+RASIKAMAHENDRAN@users.noreply.github.com>
2018-01-04 14:35:57 +0000

    Create new file

---
+++ ji.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+
+    int a[50],i;
+    printf("enter the string :");
+    scanf("%d",&a);
+
+   if(a[i]=='0'||'1')
+   {
+
+   i++;
+
+printf("yes\n");
+   }
+
+    else
+    {
+
+    i++;
+printf("no");
+    }
+
+
+}
